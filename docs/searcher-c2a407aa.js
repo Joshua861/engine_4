88daf91c3a02898b47a12c0cc2c527ca2577afbf
Lily <joshrl@proton.me>
2026-05-28 13:29:25 +0100

    rebuild docs

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7aaa51dd.js',
+                path_to_root + 'searchindex-459739ef.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
