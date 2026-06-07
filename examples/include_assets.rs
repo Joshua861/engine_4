7c22ede2270966026357fcd3a199d6f6db709164
Lily <joshrl@proton.me>
2026-06-07 11:53:15 +0100

    update include assets example

--- examples/include_assets.rs
+++ examples/include_assets.rs
@@ -1,8 +1,10 @@
-use sge::prelude::*;
+use sge::sge_include_assets;
 
 // can parse JSON, TOML, and Ron
 sge_include_assets!("assets/data", ASSETS);
 
 fn main() {
     println!("{}", ASSETS.test.messages.hello);
+    println!("{:?}", ASSETS.users.data.charlie);
+    println!("{:#?}", ASSETS.state.window);
 }
